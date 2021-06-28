bb7d1d616bc40acc5b9d64ea3251e0603d1c3f06
Farkhod Zokhidov <farafuri77@gmail.com>
2021-06-28 16:37:01 +0500

    uchinchi-vazifa

--- uchinchi-vazifa/script.js
+++ uchinchi-vazifa/script.js
@@ -8,32 +8,26 @@
 //   console.log(str + " " + n);
 // }
 
-let str = "pdp Academy";
-        // let yigindisi = 0;
+let str = "pdp academy";
+         
         let d = 0;
         let p = 0;
         for ( i = 0; i < str.length; i++) {
                 if (str.charAt(i) == 'd') {
                     d++;
-                    console.log("d ni indeksi: " + (i + 1));
+                     console.log("d ni joylashgan qatori: " + (i));
                 } else if (str.charAt(i) == 'p') {
                     p++;
-                    console.log("p ni indeksi: " + (i + 1));
+                     console.log("p ni joylashgan qatori: " + (i));
                 }
         }
         console.log("text uzunligi: "+ str.length);
         
         console.log("p: " , p);
         console.log("d: " , d);
-////////////////////////////////////
-//
+
+
        
            console.log('O`rta arifmetigi:',(p+d)/2 );
            console.log('O`rta geometrik:',(p+d)**0.5);
            console.log('Yig`indisi:' ,2+2)
-
-//dassssssssssssssssssssssss
-//dassssssssssssssssssssssss
-//dassssssssssssssssssssssss
-//dassssssssssssssssssssssss
-//dassssssssssssssssssssssss
