e789a855c925f2a6dae48727e319d5badf131803
Farkhod Zokhidov <farafuri77@gmail.com>
2021-06-28 19:16:36 +0500

    dars-03

---
+++ uchinchi-vazifa/index.js
@@ -0,0 +1,15 @@
+
+   var number = 123456
+    var str = number.toString()
+  var count = 0;
+for(var i= 0; i < str.length; i++){
+    // count += +str[i]
+ if(+str[i]%3==0){
+   count++
+ }
+}
+console.log(count)
+
+
+         
+      
