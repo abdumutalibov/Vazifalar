6eed9ddcb5331076449b21d06ec6c7e3cbdf9d61
Azimjon <dryden1309@gmail.com>
2021-06-29 23:29:14 +0500

    VAzifa

--- uchinchi-vazifa/index.js
+++ uchinchi-vazifa/index.js
@@ -1,15 +1,62 @@
 
-   var number = 123456
-    var str = number.toString()
-  var count = 0;
-for(var i= 0; i < str.length; i++){
-    // count += +str[i]
- if(+str[i]%3==0){
-   count++
- }
+//    var number = 123456
+//     var str = number.toString()
+//   var count = 0;
+// for(var i= 0; i < str.length; i++){
+//     // count += +str[i]
+//  if(+str[i]%3==0){
+//    count++
+//  }
+// }
+// console.log(count)
+function compareStr(){
+let a = 'ABC'
+let b = 'BCA'
+
+let c= a.localeCompare(b)
+console.log(c)
 }
-console.log(count)
+if(a.localeCompare(b)){
+   console.log(true)
+}else {
+console.log(false)
+}
+// if(a===b){
+//   console.log(true)
+// }else{
+//   console.log(false)
+// } 
+
+// for( let i=0;i<a.length;i++ ){
+//   for( let j=0;j<b.length;j++ ){
+//     if(a.charAt(i)===b.charAt(j)){
+//       c=
+//       console.log(true)
+//     }else{
+//       console.log(false)
+//     }
+//   }
+// }
 
 
-         
-      
+
+// unction strcmp(a, b) {
+//   a = a.toString(), b = b.toString();
+//   for (var i=0,n=Math.max(a.length, b.length); i<n && a.charAt(i) === b.charAt(i); ++i);
+//   if (i === n) return 0;
+//   return a.charAt(i) > b.charAt(i) ? -1 : 1;
+// }
+// let a = ['ABC'];
+// let b = ['BCA'];
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+
+// if (a.localeCompare(a===b)){
+//   console.log(true)
+  
+// }else{
+ 
+//   console.log(false)
+// }
