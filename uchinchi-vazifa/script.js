787d7a68e67df7b9c07d2c99bd5e36897c9d1f29
Azimjon <azimabdumutalibov21@gmail.com>
2021-08-22 00:17:43 +0500

    Uy ishi Ikkinchi topshiriq

--- uchinchi-vazifa/script.js
+++ uchinchi-vazifa/script.js
@@ -34,32 +34,75 @@
 
 
 
+// let str =['a','b','c','a','a','a','a','a',]
 
-
-let str = "pdp academy";
+let str = "AAAaHHaDDaPPPPbNNNFFFFFJJJFEEEcdefGGGGCCghijklmpn";
          
+        let a = 0;
+        let b = 0;
+        let c = 0;
         let d = 0;
+        let e = 0;
+        let f = 0;
+        let g = 0;
+        let h = 0;
+        let n = 0;
+        let j = 0;
+        let k = 0;
+        let l = 0;
+        let m = 0;
         let p = 0;
-        for ( i = 0; i < str.length; i++) {
-                if (str.charAt(i) == 'd') {
-                    d++;
-                     console.log("d ni joylashgan qatori: " + (i));
-                } else if (str.charAt(i) == 'p') {
-                    p++;
-                     console.log("p ni joylashgan qatori: " + (i));
+      
+        let set=str.toLocaleLowerCase()
+        for ( i = 0; i < set.length; i++) {
+                if (set. charAt(i) == 'a') {
+                    a++;
+                }else if (set.charAt(i) == 'p'){
+                        p++;
+                }else if (set.charAt(i) == 'd'){
+                        d++;
+                }else if (set.charAt(i) == 'c'){
+                        c++;
+                }else if (set.charAt(i) == 'e'){
+                        e++;
+                }else if (set.charAt(i) == 'f'){
+                        f++;
+                }else if (set.charAt(i) == 'g'){
+                        g++;
+                }else if (set.charAt(i) == 'h'){
+                        h++;
+                }else if (set.charAt(i) == 'n'){
+                        n++;
+                }else if (set.charAt(i) == 'j'){
+                        j++;
+                }else if (set.charAt(i) == 'k'){
+                        k++;
                 }
         }
-        console.log("text uzunligi: "+ str.length);
-        
-        console.log("p: " , p);
-        console.log("d: " , d);
+        console.log("a:",a,"d:",d,"c:",c,"e:",e,"f:",f,"g:",g,"h:",h,"n:",n,"j:",j,"p:" ,p,);
+        // console.log();
 
 
        
-           console.log('O`rta arifmetigi:',(p+d)/2 );
-           console.log('O`rta geometrik:',(p+d)**0.5);
-           console.log('Yig`indisi:' ,2+2)
+        //    console.log('O`rta arifmetigi:',(p+d)/2 );
+        //    console.log('O`rta geometrik:',(p+d)**0.5);
+        //    console.log('Yig`indisi:' ,2+2)
 
 
 
-           
+       
+        // let str = "Pdp Acaddemy  ";
+                 
+        //         let d = 0;
+        //         let p = 0;
+        //         for (let i = 0; i < str.length; i++) {
+        //                 if (str.toLowerCase().charAt(i) == ' ') {
+        //                     d++;
+        //                 //     System.out.println("d ni indexsi: " + (i + 1));
+        //                 } else if (str.toLowerCase().charAt(i) == 'p') {
+        //                 //     p++;
+        //                 //     System.out.println("p ni indexsi: " + (i + 1));
+        //                 }
+        //         }
+        //        console.log("p: " + p,"d: " + d);
+                
