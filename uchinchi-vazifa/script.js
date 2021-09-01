753bfc8458a4d8ffc226725958f680feb3358a91
Azimjon <azimabdumutalibov21@gmail.com>
2021-09-02 00:36:01 +0500

    Uy ishi gatof

--- uchinchi-vazifa/script.js
+++ uchinchi-vazifa/script.js
@@ -36,50 +36,50 @@
 
 // let str =['a','b','c','a','a','a','a','a',]
 
-let str = "AAAaHHaDDaPPPPbNNNFFFFFJJJFEEEcdefGGGGCCghijklmpn";
+// let str = "AAAaHHaDDaPPPPbNNNFFFFFJJJFEEEcdefGGGGCCghijklmpn";
          
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        let d = 0;
-        let e = 0;
-        let f = 0;
-        let g = 0;
-        let h = 0;
-        let n = 0;
-        let j = 0;
-        let k = 0;
-        let l = 0;
-        let m = 0;
-        let p = 0;
+//         let a = 0;
+//         let b = 0;
+//         let c = 0;
+//         let d = 0;
+//         let e = 0;
+//         let f = 0;
+//         let g = 0;
+//         let h = 0;
+//         let n = 0;
+//         let j = 0;
+//         let k = 0;
+//         let l = 0;
+//         let m = 0;
+//         let p = 0;
       
-        let set=str.toLocaleLowerCase()
-        for ( i = 0; i < set.length; i++) {
-                if (set. charAt(i) == 'a') {
-                    a++;
-                }else if (set.charAt(i) == 'p'){
-                        p++;
-                }else if (set.charAt(i) == 'd'){
-                        d++;
-                }else if (set.charAt(i) == 'c'){
-                        c++;
-                }else if (set.charAt(i) == 'e'){
-                        e++;
-                }else if (set.charAt(i) == 'f'){
-                        f++;
-                }else if (set.charAt(i) == 'g'){
-                        g++;
-                }else if (set.charAt(i) == 'h'){
-                        h++;
-                }else if (set.charAt(i) == 'n'){
-                        n++;
-                }else if (set.charAt(i) == 'j'){
-                        j++;
-                }else if (set.charAt(i) == 'k'){
-                        k++;
-                }
-        }
-        console.log("a:",a,"d:",d,"c:",c,"e:",e,"f:",f,"g:",g,"h:",h,"n:",n,"j:",j,"p:" ,p,);
+//         let set=str.toLocaleLowerCase()
+//         for ( i = 0; i < set.length; i++) {
+//                 if (set. charAt(i) == 'a') {
+//                     a++;
+//                 }else if (set.charAt(i) == 'p'){
+//                         p++;
+//                 }else if (set.charAt(i) == 'd'){
+//                         d++;
+//                 }else if (set.charAt(i) == 'c'){
+//                         c++;
+//                 }else if (set.charAt(i) == 'e'){
+//                         e++;
+//                 }else if (set.charAt(i) == 'f'){
+//                         f++;
+//                 }else if (set.charAt(i) == 'g'){
+//                         g++;
+//                 }else if (set.charAt(i) == 'h'){
+//                         h++;
+//                 }else if (set.charAt(i) == 'n'){
+//                         n++;
+//                 }else if (set.charAt(i) == 'j'){
+//                         j++;
+//                 }else if (set.charAt(i) == 'k'){
+//                         k++;
+//                 }
+//         }
+//         console.log("a:",a,"d:",d,"c:",c,"e:",e,"f:",f,"g:",g,"h:",h,"n:",n,"j:",j,"p:" ,p,);
         // console.log();
 
 
@@ -91,18 +91,73 @@ let str = "AAAaHHaDDaPPPPbNNNFFFFFJJJFEEEcdefGGGGCCghijklmpn";
 
 
        
-        // let str = "Pdp Acaddemy  ";
-                 
+        // let rtr = [1,2,4,4,1,5,1,7,8]
+        //          let str =rtr.join()
         //         let d = 0;
         //         let p = 0;
         //         for (let i = 0; i < str.length; i++) {
-        //                 if (str.toLowerCase().charAt(i) == ' ') {
+        //                 if (str.charAt(i) == 1) {
         //                     d++;
-        //                 //     System.out.println("d ni indexsi: " + (i + 1));
-        //                 } else if (str.toLowerCase().charAt(i) == 'p') {
-        //                 //     p++;
-        //                 //     System.out.println("p ni indexsi: " + (i + 1));
+        //                      console.log("d ni indexsi: " + (i ));
+        //                 } else if (str.charAt(i) == 4) {
+        //                      p++;
+        //                 console.log("p ni indexsi: " + (i ));
         //                 }
         //         }
         //        console.log("p: " + p,"d: " + d);
                 
+
+        
+
+//         let str =[124415178].join()
+//         let rtr =[]
+//                 //  let str =rtr
+//                  console.log(str);
+// // let str = "Pdp Acadedddmy  ";
+//         let yigindisi = 0;
+//         let d = 0;
+//         let p = 0;
+//         for (let i = 0; i < str.length; i++) {
+//                 if (str.toLowerCase().charAt(i) == '1') {
+//                     d++;
+//                     console.log("d ni indexsi: " + (i ));
+//                 } else if (str.toLowerCase().charAt(i) == '4') {
+//                     p++;
+//                     console.log("p ni indexsi: " + (i ));
+//                 }
+//         }
+//  console.log("text uzunligi: "+ str.length);
+//  console.log("yigindisi: " + yigindisi);
+//  console.log("p: " + p);
+//  console.log("d: " + d);
+
+
+
+ let arr=[162747]
+ let irr=[371241]
+let str=[...arr,...irr].sort((a,b)=>a-b).join()
+
+let list=[]
+for (value of str){
+   if(list.includes(value)){
+      console.log(value);
+   }
+   list.push(value)
+   str
+   let d = 0;
+      let p = 0;
+for (let i = 0; i < str.length; i++) {
+                        if (str.charAt(i) == '1') {
+                            d++;
+                            console.log("1 ni indexsi: " + (i ));
+                        } else if (str.charAt(i) == '4') {
+                            p++;
+                            console.log("4 ni indexsi: " + (i ));
+                        }
+                }
+        //  console.log("text uzunligi: "+ str.length);
+        // //  console.log("yigindisi: " + yigindisi);
+        //  console.log("p: " + p);
+        //  console.log("d: " + d);
+        
+}
